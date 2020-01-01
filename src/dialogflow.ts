e48bb7f4d3108ba857f899c34b6a56e783292f12
kento <kozukata1993@gmail.com>
2020-01-01 17:54:43 +0900

    dialogflow.tsを作成

---
+++ src/dialogflow.ts
@@ -0,0 +1,6 @@
+// const test = (): string[] => {
+//   const result: string[] = [];
+//   const url: string = " https://dialogflow.googleapis.com/v2/{session=projects/*/agent/sessions/*}:detectIntent";
+//   result.push("Success");
+//   return result;
+// };
